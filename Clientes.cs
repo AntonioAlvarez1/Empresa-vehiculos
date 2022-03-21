2fdefce2cfc4d14d1b59c28c3c6cd4722b9f6b9d
Anthon-c <antonioalvarez836@gmail.com>
2022-03-20 18:43:28 -0600

    Clases agregadas y Otro form

---
+++ Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa_vehiculos
+{
+    internal class Clientes
+    {
+        public string Nit { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
---
+++ Vehiculos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa_vehiculos
+{
+    internal class Vehiculos
+    {
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public decimal PrecioKilometro { get; set; }
+
+    }
+}
